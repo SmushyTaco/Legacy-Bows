9d835d5df42f18049ace44d3ae957805faef32e3
SmushyTaco <smushytaco@gmail.com>
2023-06-18 17:04:39 -0700

    1.20.1

--- Legacy Bows/src/main/java/com/smushytaco/legacy_bows/mixins/BowMixin.java
+++ Legacy Bows/src/main/java/com/smushytaco/legacy_bows/mixins/BowMixin.java
@@ -1,4 +1,7 @@
 package com.smushytaco.legacy_bows.mixins;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.smushytaco.legacy_bows.LegacyBows;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -10,24 +13,22 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(BowItem.class)
 public abstract class BowMixin {
     @Shadow
     public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {}
-    @Inject(method = "getPullProgress", at = @At("HEAD"), cancellable = true)
-    private static void hookGetPullProgress(int useTicks, CallbackInfoReturnable<Float> cir) {
-        if (!LegacyBows.INSTANCE.getConfig().getEnableLegacyBows()) return;
-        cir.setReturnValue(1.0F);
-    }
-    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
-    public void hookUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
-        if (!LegacyBows.INSTANCE.getConfig().getEnableLegacyBows()) return;
-        ItemStack itemStack = user.getStackInHand(hand);
-        boolean bl = !user.getProjectileType(itemStack).isEmpty();
-        if (!user.getAbilities().creativeMode && !bl) return;
-        onStoppedUsing(user.getStackInHand(hand), world, user, 0);
-        cir.setReturnValue(TypedActionResult.fail(itemStack));
+    @ModifyReturnValue(method = "getPullProgress", at = @At("RETURN"))
+    @SuppressWarnings("unused")
+    private static float hookGetPullProgress(float original, int useTicks) { return !LegacyBows.INSTANCE.getConfig().getEnableLegacyBows() ? original : 1.0F; }
+    @ModifyReturnValue(method = "use", at = @At("RETURN"))
+    @SuppressWarnings("unused")
+    public TypedActionResult<ItemStack> hookUse(TypedActionResult<ItemStack> original, World world, PlayerEntity user, Hand hand) { return !LegacyBows.INSTANCE.getConfig().getEnableLegacyBows() ? original : TypedActionResult.fail(user.getStackInHand(hand)); }
+    @WrapOperation(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setCurrentHand(Lnet/minecraft/util/Hand;)V"))
+    public void hookUseSetCurrentHand(PlayerEntity instance, Hand hand, Operation<Void> original, World world, PlayerEntity user, Hand handTwo) {
+        if (LegacyBows.INSTANCE.getConfig().getEnableLegacyBows()) {
+            onStoppedUsing(user.getStackInHand(handTwo), world, user, 0);
+        } else {
+            original.call(instance, hand);
+        }
     }
 }
--- Legacy Bows/src/main/java/com/smushytaco/legacy_bows/mixins/CrossbowMixin.java
+++ Legacy Bows/src/main/java/com/smushytaco/legacy_bows/mixins/CrossbowMixin.java
@@ -1,4 +1,6 @@
 package com.smushytaco.legacy_bows.mixins;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.smushytaco.legacy_bows.LegacyBows;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -10,34 +12,21 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(CrossbowItem.class)
 public abstract class CrossbowMixin {
     @Shadow
     public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {}
-    @Shadow
-    private static float getSpeed(ItemStack stack) { throw new AssertionError(); }
-    @Shadow
-    public static boolean isCharged(ItemStack stack) { throw new AssertionError(); }
-    @Shadow
-    public static void shootAll(World world, LivingEntity entity, Hand hand, ItemStack stack, float speed, float divergence) {}
-    @Shadow
-    public static void setCharged(ItemStack stack, boolean charged) {}
-    @Inject(method = "getPullProgress", at = @At("HEAD"), cancellable = true)
-    private static void hookGetPullProgress(int useTicks, ItemStack stack, CallbackInfoReturnable<Float> cir) {
-        if (!LegacyBows.INSTANCE.getConfig().getEnableLegacyCrossbows()) return;
-        cir.setReturnValue(1.0F);
-    }
-    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
-    public void hookUse(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
-        if (!LegacyBows.INSTANCE.getConfig().getEnableLegacyCrossbows()) return;
+    @ModifyReturnValue(method = "getPullProgress", at = @At("RETURN"))
+    @SuppressWarnings("unused")
+    private static float hookGetPullProgress(float original, int useTicks) { return !LegacyBows.INSTANCE.getConfig().getEnableLegacyCrossbows() ? original : 1.0F; }
+    @ModifyReturnValue(method = "use", at = @At("RETURN"))
+    public TypedActionResult<ItemStack> hookUse(TypedActionResult<ItemStack> original, World world, PlayerEntity user, Hand hand) {
+        if (!LegacyBows.INSTANCE.getConfig().getEnableLegacyCrossbows()) return original;
         ItemStack itemStack = user.getStackInHand(hand);
-        if (isCharged(itemStack)) {
-            shootAll(world, user, hand, itemStack, getSpeed(itemStack), 1.0F);
-            setCharged(itemStack, false);
-        }
         onStoppedUsing(itemStack, world, user, 0);
-        cir.setReturnValue(TypedActionResult.fail(itemStack));
+        return TypedActionResult.fail(itemStack);
     }
+    @ModifyExpressionValue(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z"))
+    @SuppressWarnings("unused")
+    private boolean hookUseIsEmpty(boolean original) { return LegacyBows.INSTANCE.getConfig().getEnableLegacyCrossbows() || original; }
 }
