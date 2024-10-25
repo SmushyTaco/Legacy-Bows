e5030cc9d02f8b3f32adc035f7f8a54bef3440c1
Nikan Radan <github.com@alias.nikanradan.com>
2024-10-24 17:31:36 -0700

    1.21.3

--- Legacy Bows/src/main/java/com/smushytaco/legacy_bows/mixins/CrossbowMixin.java
+++ Legacy Bows/src/main/java/com/smushytaco/legacy_bows/mixins/CrossbowMixin.java
@@ -6,8 +6,8 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,15 +15,15 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(CrossbowItem.class)
 public abstract class CrossbowMixin {
     @Shadow
-    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {}
+    public abstract boolean onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks);
     @ModifyReturnValue(method = "getPullProgress", at = @At("RETURN"))
     private static float hookGetPullProgress(float original, int useTicks) { return !LegacyBows.INSTANCE.getConfig().getEnableLegacyCrossbows() ? original : 1.0F; }
     @ModifyReturnValue(method = "use", at = @At("RETURN"))
-    public TypedActionResult<ItemStack> hookUse(TypedActionResult<ItemStack> original, World world, PlayerEntity user, Hand hand) {
+    public ActionResult hookUse(ActionResult original, World world, PlayerEntity user, Hand hand) {
         if (!LegacyBows.INSTANCE.getConfig().getEnableLegacyCrossbows()) return original;
         ItemStack itemStack = user.getStackInHand(hand);
         onStoppedUsing(itemStack, world, user, 0);
-        return TypedActionResult.fail(itemStack);
+        return ActionResult.FAIL;
     }
     @ModifyExpressionValue(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z"))
     private boolean hookUseIsEmpty(boolean original) { return LegacyBows.INSTANCE.getConfig().getEnableLegacyCrossbows() || original; }
